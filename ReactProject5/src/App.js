e49d8611684162d97b647d3ea138a177722762d1
RayAlcala <101369756+RayAlcala@users.noreply.github.com>
2022-04-11 16:11:55 -0700

    ReactProject5

---
+++ ReactProject5/src/App.js
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
+
+function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+  // initial state is set to false bc we do not want to see the cart popup as a default 
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
+  return (
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler}/>}
+      {/* only render cart component (the popup) when cartIsShown is true
+      and when popup is closed */}
+
+      {/* notes on onClose:
+      we want to also close cart popup by clicking on backdrop
+      notice how this is a cart component. go to Cart.js
+      notice how there is a modal component in Cart.js. go to Modal.js 
+      modal component is also used in Cart.js. go to Cart.js*/}
+      <Header onShowCart={showCartHandler} />
+      {/* notice this is a header component
+      props is set up in Header.js. go down rabbit hole like we did above  */}
+      <main>
+          <Meals />
+      </main>
+    </CartProvider>
+  );
+}
+
+export default App;
